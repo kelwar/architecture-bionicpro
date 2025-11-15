12dcc3f263b266899805bb9e76f2fbc4e9e86c5d
Artem Ufimtsev <kelwar@elbrus.ru>
2025-11-15 19:01:04 +0300

    data in crm_db

--- air-flow/dags/sql/create_telemetry_table.sql
+++ air-flow/dags/sql/create_telemetry_table.sql
@@ -3,6 +3,6 @@ create table if not exists telemetry (
     timestamp Datetime,
     value Decimal(15,5)
 )
-ENGINE = ReplacingMergeTree
-partition by toDate(timestamp)
+ENGINE = ReplacingMergeTree()
+partition by toYYYYMM(timestamp)
 order by (sensor_id, timestamp)
