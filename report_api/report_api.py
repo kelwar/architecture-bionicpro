08054b98876e70948adb62a8e296b15c1be3c8fa
Artem Ufimtsev <kelwar@elbrus.ru>
2025-11-15 20:17:30 +0300

    report-api

--- report_api/report_api.py
+++ report_api/report_api.py
@@ -2,24 +2,24 @@ import os
 from typing import Annotated
 
 import clickhouse_connect
-from fastapi import FastAPI, Header
 import jwt
-# import traceback
+from fastapi import FastAPI, Header
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
-print(f"app: {app}")
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 
 @app.get("/reports")
 def getReport(token: Annotated[str | None, Header()]):
-    try:
-        payload = jwt.decode(token, "kjfaeuswy3rrbfu32f982bf4398453b4tg98r3h", algorithms="HS256")
-        person_id = payload.get("person_id")
+    payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms="HS256")
+    person_id = payload.get("person_id")
+    print(f"person_id: {person_id}")
 
-        client = clickhouse_connect.get_client(host=os.getenv("CLICKHOUSE_HOST"), port=int(os.getenv("CLICKHOUSE_PORT")),
-                                               username=os.getenv("CLICKHOUSE_USER"), password=os.getenv("CLICKHOUSE_PASS"))
-        result = client.query(query='SELECT * FROM report WHERE person_id = %(person_id)s',
-                              parameters={ "person_id": person_id })
-        return result
-    except Exception:
-        print("Exception")
-        # traceback.print_exc()
+    client = clickhouse_connect.get_client(host=os.getenv("CLICKHOUSE_HOST"), port=int(os.getenv("CLICKHOUSE_PORT")),
+                                           username=os.getenv("CLICKHOUSE_USER"), password=os.getenv("CLICKHOUSE_PASS"))
+    result = client.query(query='SELECT * FROM report WHERE person_id = %(person_id)s',
+                          parameters={"person_id": person_id})
+    return result
