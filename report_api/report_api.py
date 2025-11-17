a73f96920e39679f71751ff2b537fa5b76082310
Artem Ufimtsev <kelwar@elbrus.ru>
2025-11-18 00:02:39 +0300

    email

--- report_api/report_api.py
+++ report_api/report_api.py
@@ -1,5 +1,4 @@
 import os
-from collections import namedtuple
 from typing import Annotated
 
 import clickhouse_connect
@@ -23,22 +22,14 @@ def root():
 
 @app.get("/reports")
 async def getReport(authorization: Annotated[str | None, Header()]):
-    print(f"Authorization: {authorization}")
     token = str.replace(authorization, "Bearer ", "")
-    print(f"token: {token}")
     user_info = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=["HS256"], options={"verify_signature": False})
-    print(f"user: {user_info}")
     email = user_info.get("email")
     print(f"email: {email}")
 
     client = clickhouse_connect.get_client(host=os.getenv("CLICKHOUSE_HOST"), port=int(os.getenv("CLICKHOUSE_PORT")),
                                            username=os.getenv("CLICKHOUSE_USER"), password=os.getenv("CLICKHOUSE_PASS"))
-    print(f"client: {client}")
-    result = client.query(query='SELECT * FROM report WHERE email = %(email)s',
-                          parameters={"email": email})
-    print(f"result: {result}")
-
-    Report = namedtuple("Report", ["sensor_id", "sensor_type", "timestamp", "value",
-                         "product_id", "product_type", "person_id", "last_name", "first_name", "patronymic",
-                                   "birthday", "email"])
-    return [Report(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11]) for r in result.result_rows]
+    result = client.query(query='SELECT * FROM report WHERE email = %(email)s', parameters={"email": email})
+    return [{"sensor_id": r[0], "sensor_type": r[1], "timestamp": r[2], "value": r[3],
+             "product_id": r[4], "product_type": r[5], "person_id": r[6], "last_name": r[7], "first_name": r[8], "patronymic": r[9],
+             "birthday": r[10], "email": r[11]} for r in result.result_rows]
