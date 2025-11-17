e723c0536064c842d5de7dea68bc46a4fc367f98
Artem Ufimtsev <kelwar@elbrus.ru>
2025-11-17 23:56:23 +0300

    email

--- report_api/report_api.py
+++ report_api/report_api.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple
 from typing import Annotated
 
 import clickhouse_connect
@@ -32,6 +33,12 @@ async def getReport(authorization: Annotated[str | None, Header()]):
 
     client = clickhouse_connect.get_client(host=os.getenv("CLICKHOUSE_HOST"), port=int(os.getenv("CLICKHOUSE_PORT")),
                                            username=os.getenv("CLICKHOUSE_USER"), password=os.getenv("CLICKHOUSE_PASS"))
+    print(f"client: {client}")
     result = client.query(query='SELECT * FROM report WHERE email = %(email)s',
                           parameters={"email": email})
-    return result
+    print(f"result: {result}")
+
+    Report = namedtuple("Report", ["sensor_id", "sensor_type", "timestamp", "value",
+                         "product_id", "product_type", "person_id", "last_name", "first_name", "patronymic",
+                                   "birthday", "email"])
+    return [Report(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11]) for r in result.result_rows]
